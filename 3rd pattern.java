81ccda4f295cc57fc26d0d232ef55256670bb516
Deepa091217 <23bm006@excelcolleges.com>
2026-07-17 16:07:52 +0530

    Create 3rd pattern.java

---
+++ 3rd pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter n :");
+        int n = sc.nextInt();
+        for(int i = 1; i<=3; i++){
+            for (int j=1; j<=3; j++){
+            if(i==j || i==n-j+1){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            }
+            System.out.println();
+    }
+}
+}
