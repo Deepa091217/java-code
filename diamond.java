42de56f78fb0546c520b0b1f2e590b9a86997af3
Deepa091217 <23bm006@excelcolleges.com>
2026-07-27 17:59:29 +0530

    Create diamond.java

---
+++ diamond.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n:");
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=n-i;j++)
+            System.out.print(" ");
+            for(int k = 1;k<=n-i;k++)
+            System.out.print(" * ");
+           
+        System.out.println();
+    }
+        for(int i =n-1;i>=n;i++){
+        for(int j = 1;j<=n-i;j++)
+            System.out.print(" ");
+            for(int k = 1;k<=n-i;k++)
+            System.out.print(" * ");
+            
+    System.out.println();
+}
+}
+}
