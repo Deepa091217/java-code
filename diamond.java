a27d48efbae39935b81ce6eba617ab1f30a9bebc
Deepa091217 <23bm006@excelcolleges.com>
2026-07-28 16:26:22 +0530

    Update diamond.java

--- diamond.java
+++ diamond.java
@@ -7,16 +7,16 @@ class Main {
         for(int i = 1;i<=n;i++){
         for(int j = 1;j<=n-i;j++)
             System.out.print(" ");
-            for(int k = 1;k<=n-i;k++)
-            System.out.print(" * ");
+            for(int k = 1;k<=(2*i-1);k++)
+            System.out.print("*");
            
         System.out.println();
     }
-        for(int i =n-1;i>=n;i++){
+    for(int i =n-1;i>=1;i--){
         for(int j = 1;j<=n-i;j++)
             System.out.print(" ");
-            for(int k = 1;k<=n-i;k++)
-            System.out.print(" * ");
+            for(int k = 1;k<=(2*i-1);k++)
+            System.out.print("*");
             
     System.out.println();
 }
