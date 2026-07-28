656b52dd322861c2ecb7aadfdeb065687825ad7a
Deepa091217 <23bm006@excelcolleges.com>
2026-07-28 16:02:27 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class RotateTheArray{
+  public static void main (String[] args) {
+        int [] arr = {1,2,3,4,5};
+        int Temp =arr[0];
+        for (int i =0; i < arr.length-1;i++){
+            arr[i] = arr[i+1];
+        }
+        arr[arr.length-1] = Temp;
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i] + "");
+        }
+    }
+}
